02c369a50dd2eb67ee0d423d843a2758b41eb9a5
aasifreact <aasifreact@gmail.com>
2024-07-30 14:55:34 -0700

    updated the star of rating

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,21 +2,19 @@ import React from 'react';
 import '../components/MovieList.css';
 
 const MovieList = ({ movies }) => {
-  const uniqueMovies = movies.reduce((acc, current) => {
-    if (!acc.find(movie => movie.id === current.id)) {
-      acc.push(current);
-    }
-    return acc;
-  }, []);
-
   return (
     <div className="movie-list">
-      {uniqueMovies.map((movie, index) => (
-        <div key={`${movie.id}-${index}`} className="movie-card">
+      {movies.map(movie => (
+        <div key={movie.id} className="movie-card">
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
           <h3>{movie.title}</h3>
           <div className="rating">
-            <p>Rating: {movie.vote_average}</p>
+            {Array.from({ length: 5 }, (_, index) => (
+              <span key={index} className="star">
+                {index < Math.round(movie.vote_average / 2) ? '★' : '☆'}
+              </span>
+            ))}
+            <span className="rating-text">{movie.vote_average.toFixed(1)}</span>
           </div>
         </div>
       ))}
